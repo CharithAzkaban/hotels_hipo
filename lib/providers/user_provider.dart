cb33263e9617917868ad2818ded1a70fe4d9569d
Charith Madushan <44424769+CharithAzkaban@users.noreply.github.com>
2023-01-12 08:03:24 +0530

    fixed

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,6 +1,4 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:hotels_hipo/common/consts.dart';
 import 'package:hotels_hipo/models/user.dart';
 
 class UserProvider extends ChangeNotifier {
